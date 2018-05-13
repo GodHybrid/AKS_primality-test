4ec0cab938c32981456c731b891f3ea15ba5490b
GodHybrid <6216442+GodHybrid@users.noreply.github.com>
2018-05-14 00:36:27 +0300

    Add files via upload

--- AKS_algorithm/Form1.cs
+++ AKS_algorithm/Form1.cs
@@ -15,7 +15,9 @@ namespace AKS_algorithm
     public partial class Form1 : Form
     {
         static BigInteger[] c = new BigInteger[10000];
+        String Equation = String.Empty;
         private Thread t;
+        static Thread MainThread = null;
 
         public Form1()
         {
@@ -24,7 +26,7 @@ namespace AKS_algorithm
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            MainThread = Thread.CurrentThread;
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -42,6 +44,7 @@ namespace AKS_algorithm
         private void button1_Click(object sender, EventArgs e)
         {
             ushort TryRes;
+            textBox1.Text += "Primes:\r\n";
             try
             {
                 UInt16.TryParse(textBox3.Text, out TryRes);
@@ -58,9 +61,9 @@ namespace AKS_algorithm
             else
             {
                 SwitchEnabledStates();
+                textBox1.Text = "Primes:\r\n";
                 t = new Thread(StartAKS);
                 t.Start();
-                AKS(TryRes);
             }
         }
 
@@ -68,22 +71,26 @@ namespace AKS_algorithm
         {
             ushort N = UInt16.Parse(textBox3.Text);
             AKS(N);
-            t.Abort();
         }
 
         void AKS(ushort n)
         {
-            for (int i = 0; i < n; i++)
-            {
-                coef(i);
-                textBox2.Text += "(x-1)^" + i + " = ";
-                show(i);
-                textBox2.Text += "\r\n";
-            }
-            textBox1.Text += "Primes:\r\n";
             for (int i = 1; i <= n; i++)
-                if (is_prime(i))
-                    textBox1.Text += i + " ";
+            {
+                String tmp = String.Empty;
+                coef(i);
+                Equation += "(x-1)^" + i + " =";
+                show(i);
+                tmp = Equation;
+                Equation = String.Empty;
+                textBox2.Invoke( (Action)(() => { textBox2.Text += tmp; }) );
+                //Task.Run( () => { textBox2.Text += tmp; } );
+                if (i > 1 && is_prime(i))
+                {
+                    textBox1.Invoke( (Action)(() => { textBox1.Text += i + " "; }) );
+                }
+            }
+            t.Abort();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -115,23 +122,29 @@ namespace AKS_algorithm
 
         static bool is_prime(int n)
         {
-            int i;
+            if (n >= 1)
+            {
+                int i;
 
-            coef(n);
-            c[0] += 1;
-            c[i = n] -= 1;
+                coef(n);
+                c[0] += 1;
+                c[i = n] -= 1;
 
-            while (i-- != 0 && (c[i] % n) == 0) ;
+                while (i-- != 0 && (c[i] % n) == 0) ;
 
-            return i < 0;
+                return i < 0;
+            }
+            else return false;
         }
 
         void show(int n)
         {
             do
             {
-                textBox2.Text += "+" + c[n] + "x^" + n;
+                if (c[n] < 0) Equation += " - " + c[n] + "x^" + n;
+                else Equation += " + " + c[n] + "x^" + n;
             } while (n-- != 0);
+            Equation += "\r\n";
         }
     }
 }
